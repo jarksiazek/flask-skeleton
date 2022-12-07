f72b11ec2a10f18ed59585f4936e07e6783f0c08
jarksiazek <jarksiazek@gmail.com>
2022-12-07 09:09:04 +0100

    init commit

--- src/code/const.py
+++ src/code/const.py
@@ -1,5 +1,15 @@
 from typing import Union
 from flask import Flask
+from flask_restx import Api
+from flask import Blueprint
+
+swagger: Blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
+api: Api = Api(
+    swagger,
+    version="0.1",
+    title="api",
+    doc="/swagger/"
+)
 
 
 def create_app(config_object: Union[object, str]) -> Flask:
--- src/code/view.py
+++ src/code/view.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-
+from flask import render_template
 from src.code.logger import create_logger
 
 logger = create_logger(__name__)
@@ -9,4 +9,4 @@ blueprint: Blueprint = Blueprint("flask-skeleton", __name__)
 
 @blueprint.route("/")
 def home():
-    return "works"
+    return render_template("base_page.html")
